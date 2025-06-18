1cbfb14f7ef000480bfebe7af550973b3017d0b5
Lisa <lalisssa.monobal@gmail.com>
2025-06-18 20:10:57 +0300

    initial code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [weather, setWeather] = useState(null); 
+  const [error, setError] = useState(null);     
+  
+  const getWeather = async () => {
+    try {
+      
+      const response = await fetch(
+        'https://api.openweathermap.org/data/2.5/weather?q=London&appid=baa477efc19ae61265429bc7dec0999e&units=metric'
+      );
+
+      
+      if (!response.ok) {
+        throw new Error('Failed to receive the data');
+      }
+
+      
+      const data = await response.json();
+
+      setWeather(data.main.temp);
+      setError(null); 
+    } catch (err) {
+      setError(err.message);
+      setWeather(null);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>Weather</h1>
+      <button onClick={getWeather}>Tap to see the temperature</button>
+      {error ? (
+        <p className="visible-p">{error}</p> 
+      ) : weather ? (
+        <h2 className="visible">Temperature in London: {weather}°C</h2>
+      ) : (
+        <p className='visible-p'>Tap to see the temperature</p>
+      )}
     </div>
   );
 }
